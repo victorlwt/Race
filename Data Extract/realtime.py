8948969dec46d36794de493f3179e8c545af7dd3
Victor <lamwingtok@gmail.com>
2019-07-14 17:02:33 +0800

    Realtime added

---
+++ Data Extract/realtime.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+import pickle as pk
+import time
+import os
+
+global T1, T2, T3
+
+def getBasePage(url):
+	global T1, T2, T3
+	dr = webdriver.PhantomJS(executable_path='phantomjs.exe')
+	dr.get(url)
+	time.sleep(2)
+	try:
+		T1 = BeautifulSoup(dr.find_element_by_id("wpTable1").get_attribute("innerHTML"))
+		T2 = BeautifulSoup(dr.find_element_by_id("combOddsTableQIN").get_attribute("innerHTML"))
+		T3 = BeautifulSoup(dr.find_element_by_id("combOddsTableQPL").get_attribute("innerHTML"))
+		dr.close()
+	except:
+		dr.close()
+
+def getData():
+	global T1, T2, T3
+	table = T1.find_all("table")[1]
+	df = pd.read_html(table.prettify())[0]
+	df = df.drop(1, axis=1).drop(2, axis=1).drop(15, axis=0)
+	with open("realtimewin.df", "wb") as f:
+		pk.dump(df, f)
+
+	table = T2.find("table")
+	df = pd.read_html(table.prettify())[0]
+	with open("realtimeQIN.df", "wb") as f:
+		pk.dump(df, f)
+
+	table = T3.find("table")
+	df = pd.read_html(table.prettify())[0]
+	with open("realtimeQPL.df", "wb") as f:
+		pk.dump(df, f)
+
+
+getBasePage("https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=en&dv=local")
+getData()
